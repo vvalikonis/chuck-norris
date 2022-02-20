fad0178486baea4b1a6994fb6f446fdcc37d206c
vaidas.valikonis <vvalikonis@gmail.com>
2022-02-20 13:12:35 +0200

    Added cross-origin.

--- src/main/java/com/juniorjavadeveloper/chuckfacts/ChuckNorrisController.java
+++ src/main/java/com/juniorjavadeveloper/chuckfacts/ChuckNorrisController.java
@@ -1,13 +1,11 @@
 package com.juniorjavadeveloper.chuckfacts;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("")
+@CrossOrigin("*")
 public class ChuckNorrisController {
 
     private final ChuckNorrisFactory chuckNorrisFactory;
